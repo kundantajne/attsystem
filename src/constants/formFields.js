491507e00fc9c492bfb845b4dd908d9872186698
msd5374 <mukkawar33@gmail.com>
2022-08-04 12:35:09 +0530

    added Reg & Login

---
+++ src/constants/formFields.js
@@ -0,0 +1,67 @@
+const loginFields=[
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    }
+]
+
+const signupFields=[
+    {
+        labelText:"Enter Name",
+        labelFor:"Enter Name",
+        id:"name",
+        name:"name",
+        type:"text",
+        autoComplete:"Enter Name",
+        isRequired:true,
+        placeholder:"Enter Name"   
+    },
+    {
+        labelText:"Email address",
+        labelFor:"email-address",
+        id:"email-address",
+        name:"email",
+        type:"email",
+        autoComplete:"email",
+        isRequired:true,
+        placeholder:"Email address"   
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"   
+    },
+    {
+        labelText:"Confirm Password",
+        labelFor:"confirm-password",
+        id:"confirm-password",
+        name:"confirm-password",
+        type:"password",
+        autoComplete:"confirm-password",
+        isRequired:true,
+        placeholder:"Confirm Password"   
+    }
+]
+
+export {loginFields,signupFields}
